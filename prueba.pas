67bc29f975cf5fa4e136b4ab4938f7bd954fcd6b
Mario Caballero <mario.caballero.948@gmail.com>
2024-12-05 15:14:00 -0300

    refactor: remove unused test1 procedure from prueba.pas; update output message for clarity

--- prueba.pas
+++ prueba.pas
@@ -3,37 +3,6 @@ Program prueba;
 
 Uses crt;
 
-Procedure test1(Var leg: String);
-
-Var 
-  key: Char;
 Begin
-  Repeat
-    key := readkey;
-    If (key <> chr(27)) Then
-      If (key = chr(8)) Then
-        Begin
-          clrscr;
-          // Para quitarle uno al string uso la funcion Delete
-          Delete(leg, Length(leg), 1);
-          write('leg: ', leg);
-        End
-    Else
-      Begin
-        clrscr;
-        leg := leg + key;
-        write('leg: ', leg);
-      End
-  Until key = chr(27);
-End;
-
-Var 
-  leg: String;
-
-Begin
-  leg := '';
-  write('leg: ');
-  test1(leg);
-  writeln('Saliste');
-  writeln(leg);
+  WriteLn('Archivo para pruebas de units')
 End.
